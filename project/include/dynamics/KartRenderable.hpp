74fe9efd213a390a02934498d50c623aede2cc54
goubetc <claude.goubet2@gmail.com>
2016-04-11 16:28:31 +0200

    changed kart to kart renderable!

---
+++ project/include/dynamics/KartRenderable.hpp
@@ -0,0 +1,55 @@
+#ifndef KART_RENDERABLE_HPP
+#define KART_RENDERABLE_HPP
+
+#include "../HierarchicalRenderable.hpp"
+#include "Particle.hpp"
+#include "DynamicSystem.hpp"
+#include "DynamicSystemRenderable.hpp"
+#include "ParticleRenderable.hpp"
+#include "../FloorRenderable.hpp"
+#include <vector>
+#include <glm/glm.hpp>
+
+
+
+
+/**@brief Render a particle to the screen.
+ *
+ * Render a particle to the screen. Since a particle is modeled by
+ * a ball, this renderable simply render the corresponding ball. If
+ * you have more than one renderable, have a look to ParticleListRenderable.
+ */
+
+class KartRenderable : public HierarchicalRenderable
+{
+public:
+std::shared_ptr<ParticleRenderable> master;        
+  ~KartRenderable();
+  /**@brief Build a particle renderable.
+   *
+   * Build a renderable to render a particle.
+   * @param program The shader program used to render the particle.
+   * @param particle The particle to render.
+   */
+  KartRenderable( ShaderProgramPtr program, ParticlePtr mobile );
+
+
+private:
+  void do_draw();
+  void do_animate( float time );
+
+
+
+  ParticlePtr m_particle;
+std::vector< glm::vec3 > m_positions;
+  std::vector< glm::vec4 > m_colors;
+  std::vector< glm::vec3 > m_normals;
+
+  unsigned int m_pBuffer;
+  unsigned int m_cBuffer;
+  unsigned int m_nBuffer;
+};
+
+typedef std::shared_ptr<KartRenderable> KartRenderablePtr;
+
+#endif //KART_RENDERABLE_HPP
