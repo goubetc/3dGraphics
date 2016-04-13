9376c03679e9e27337f1404501c27455f078b4ed
Claude Goubet <goubetc@ensipc308.imag.fr>
2016-04-13 21:18:43 +0200

    fixed floor + merged forces

--- project/src/setup_kart.cpp
+++ project/src/setup_kart.cpp
@@ -41,7 +41,7 @@
 
 #include "../include/BillboardRenderable.hpp"
 
-#include "./../include/MeshRenderable.hpp"
+#include "./../include/HierarchicalMeshRenderable.hpp"
 
 #include <iostream>
 #include <string>
@@ -92,15 +92,15 @@ system->setRestitution(1.0f);
 
   
  //hierarchical_kart( viewer, system, systemRenderable );
-  std::shared_ptr<MeshRenderable> road = std::make_shared<MeshRenderable>(flatShader, "./../meshes/track.obj");
+  std::shared_ptr<HierarchicalMeshRenderable> road = std::make_shared<HierarchicalMeshRenderable>(flatShader, "./../meshes/track.obj");
 
-  //  road->setLocalTransform( GeometricTransformation( glm::vec3{5,5,5.5},
-								 // glm::angleAxis( float(M_PI/2), glm::normalize(glm::vec3( 0,1,0)) ),
-								 // glm::vec3{1,1,1}).toMatrix() );
+  road->setLocalTransform( GeometricTransformation( glm::vec3{0,0,19},
+						    glm::angleAxis( float(M_PI/2), glm::normalize(glm::vec3( 1,0,0)) ),
+						    glm::vec3{20,20,20}).toMatrix() );
  
   viewer.addRenderable(road);
   
- //Finally activate animation
+  //Finally activate animation
  viewer.startAnimation();
 }
 
@@ -166,9 +166,9 @@ void setup_lights(Viewer& viewer){
 void setup_kart(Viewer& viewer, DynamicSystemPtr& system, DynamicSystemRenderablePtr &systemRenderable){
   //Initialize Kart with position, velocity, mass and radius and add it to the system
   ShaderProgramPtr flatShader = std::make_shared<ShaderProgram>("../shaders/flatVertex.glsl","../shaders/flatFragment.glsl");
-  glm::vec3 px(0.0,0.0,0.0),pv(0.0,0.0,0.0);
+  glm::vec3 px(0.0,10.0,0.0),pv(0.0,0.0,0.0);
   float pm=1.0, pr=1.0;
-  px = glm::vec3(0.0,0.0,1.0);
+  px = glm::vec3(20.0,20.0,1.0);
 
   ParticlePtr mobile = std::make_shared<Particle>( px, pv, pm, pr);
 
