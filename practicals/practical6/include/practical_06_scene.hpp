4d5e0c51160f29d94ba66b332d1e2ca5069223f4
Eline Beslic <beslice@ensipsys14.imag.fr>
2016-04-14 14:42:39 +0200

    remove practicals

--- practicals/practical6/include/practical_06_scene.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef PRACTICAL_06_SCENE_HPP
-#define PRACTICAL_06_SCENE_HPP
-
-#include "Viewer.hpp"
-
-void initialize_practical_06_scene( Viewer& viewer );
-
-#endif 
--- practicals/practical6/src/dynamics/Solver.cpp
+++
@@ -1,6 +0,0 @@
-# include "../../include/dynamics/Solver.hpp"
-
-void Solver::solve( const float& dt, std::vector<ParticlePtr>& particles )
-{
-  do_solve( dt, particles );
-}
